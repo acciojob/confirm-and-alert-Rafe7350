fa0aaba73bda56bf8faa52a39ef648e6be3f6995
Rafe7350 <117887198+Rafe7350@users.noreply.github.com>
2023-05-08 11:22:00 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
 //your JS code here. If required.
+var a =conform("Do you want to proceed?");
+if () {
+	alert(“You clicked OK. Proceeding...”);
+}else{
+	alert("You clicked Cancel. Exiting...");
+}
